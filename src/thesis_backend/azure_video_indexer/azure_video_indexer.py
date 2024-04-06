47e94c0cf733ff5b5a1212505797909de3dbe8ab
Jędrzej Głowaczewski <jedrzej.glowaczewski@gmail.com>
2024-04-06 23:37:40 +0200

    Store videos status

--- src/thesis_backend/azure_video_indexer/azure_video_indexer.py
+++ src/thesis_backend/azure_video_indexer/azure_video_indexer.py
@@ -1,9 +1,11 @@
 import time
-import requests
+from typing import NamedTuple, Mapping
 
+import requests
 
 LOCATION = "trial"
 TOKEN_EXPIRATION = 59 * 60  # 59 minutes
+VIDEO_LIST_REFRESH = 1  # 1 second
 
 
 class VideoIndexerToken:
@@ -29,21 +31,63 @@ class VideoIndexerToken:
         return response.json()
 
 
+class VideoStatus(NamedTuple):
+    url: str
+    state: str
+    progress: int
+
+
+class AzureVideoCatalog:
+    def __init__(self, account_id: str, primary_key: str, access_token: VideoIndexerToken) -> None:
+        self.__account_id = account_id
+        self.__access_token = access_token
+        self.__headers = {"Ocp-Apim-Subscription-Key": primary_key}
+        self.__videos: Mapping[str, VideoStatus] = {}
+        self.__next_refresh = -1
+
+    def __refresh_videos(self) -> None:
+        url = f"https://api.videoindexer.ai/{LOCATION}/Accounts/{self.__account_id}/Videos"
+        query_params = {"accessToken": self.__access_token.token}
+        response = requests.get(url, params=query_params, headers=self.__headers)
+        videos = response.json()["results"]
+        self.__videos = {}
+        for video in videos:
+            video_url = video["description"]
+            video_state = video["state"]
+            # Remove '%' from the end of the string and convert to int
+            video_progress = int(video["processingProgress"][:-1])
+            if video_url:
+                self.__videos[video_url] = (VideoStatus(video_url, video_state, video_progress))
+        return
+
+    @property
+    def videos(self) -> Mapping[str, VideoStatus]:
+        current_time = time.time()
+        if self.__next_refresh < current_time:
+            self.__next_refresh = current_time + VIDEO_LIST_REFRESH
+            self.__refresh_videos()
+        return self.__videos
+
+
 class AzureVideoIndexer:
     def __init__(self, account_id: str, primary_key: str) -> None:
         self.__account_id = account_id
         self.__access_token = VideoIndexerToken(account_id, primary_key)
         self.__headers = {"Ocp-Apim-Subscription-Key": primary_key}
+        self.__video_catalog = AzureVideoCatalog(account_id, primary_key, self.__access_token)
 
-    def upload_video(self, video_url: str, video_name: str) -> dict:
+    def upload_video(self, video_url: str, video_name: str) -> VideoStatus:
+        if video_url in self.__video_catalog.videos:
+            return self.__video_catalog.videos[video_url]
         url = f"https://api.videoindexer.ai/{LOCATION}/Accounts/{self.__account_id}/Videos"
         query_params = {
             "accessToken": self.__access_token.token,
             "videoUrl": video_url,
-            "name": video_name
+            "name": video_name,
+            "description": video_url
         }
-        response = requests.post(url, params=query_params, headers=self.__headers)
-        return response.json()
+        response = requests.post(url, params=query_params, headers=self.__headers).json()
+        return VideoStatus(video_url, response["state"], response["processingProgress"])
 
     def get_video_index(self, video_id: str) -> dict:
         url = f"https://api.videoindexer.ai/{LOCATION}/Accounts/{self.__account_id}/Videos/{video_id}/Index"
@@ -51,7 +95,6 @@ class AzureVideoIndexer:
         response = requests.get(url, params=query_params, headers=self.__headers)
         return response.json()
 
-    # def get_video_index_by_id(self, video_id):
-    #     url = f"https://api.videoindexer.ai/{LOCATION}/Accounts/{self.account_id}/Videos/{video_id}/Index?accessToken={self.access_token}"
-    #     response = requests.get(url, headers=self.headers)
-    #     return response.json()
+    def get_videos_status(self, urls: list[str]) -> Mapping[str, VideoStatus]:
+        filtered_videos = {k: v for k, v in self.__video_catalog.videos.items() if k in urls}
+        return filtered_videos
--- tests/test_azure_video_indexer.py
+++ tests/test_azure_video_indexer.py
@@ -1,41 +1,68 @@
 import os
+import unittest
+
 from dotenv import load_dotenv
 
-from src.thesis_backend.azure_video_indexer.azure_video_indexer import AzureVideoIndexer, VideoIndexerToken
+from src.thesis_backend.azure_video_indexer.azure_video_indexer import AzureVideoIndexer, VideoIndexerToken, \
+    AzureVideoCatalog
 
 load_dotenv(os.path.join(os.path.dirname(__file__), "../.env"))
 
 
-def test_get_access_token():
-    account_id = os.environ.get("VIDEO_INDEXER_ACCOUNT_ID")
-    primary_key = os.environ.get("VIDEO_INDEXER_PRIMARY_KEY")
-    azure_video_indexer = VideoIndexerToken(account_id, primary_key)
-    token = azure_video_indexer.token
-    assert token is not None
-    assert isinstance(token, str)
+class TestAzureVideoCatalog(unittest.TestCase):
+    def setUp(self):
+        self.account_id = os.environ.get("VIDEO_INDEXER_ACCOUNT_ID")
+        self.primary_key = os.environ.get("VIDEO_INDEXER_PRIMARY_KEY")
+        self.video_url = "https://storage.googleapis.com/videostrates.appspot.com/files/f90454fd-1616-4845-8252-f2978bab22a7.mp4"
+        self.video_name = "test_video"
+        self.video_id = "84505873fd"
 
+    def test_get_access_token(self):
+        azure_token = VideoIndexerToken(self.account_id, self.primary_key)
+        token = azure_token.token
+        print(token)
+        assert token is not None
+        assert isinstance(token, str)
 
-def test_upload_video():
-    account_id = os.environ.get("VIDEO_INDEXER_ACCOUNT_ID")
-    primary_key = os.environ.get("VIDEO_INDEXER_PRIMARY_KEY")
-    azure_video_indexer = AzureVideoIndexer(account_id, primary_key)
-    video_url = "https://storage.googleapis.com/videostrates.appspot.com/files/f90454fd-1616-4845-8252-f2978bab22a7.mp4"
-    video_name = "sample-mp4-file"
-    response = azure_video_indexer.upload_video(video_url, video_name)
-    assert response is not None
-    assert isinstance(response, dict)
-    assert "id" in response
-    assert "state" in response
-    assert response["state"] == "Uploaded"
+    def test_video_catalog(self):
+        azure_token = VideoIndexerToken(self.account_id, self.primary_key)
+        video_catalog = AzureVideoCatalog(self.account_id, self.primary_key, azure_token)
+        videos = video_catalog.videos
+        assert videos is not None
+        assert isinstance(videos, dict)
+        assert len(videos) > 0
+        for url, video in videos.items():
+            assert video.url is not None
+            assert url == video.url
+            assert video.state is not None
+            assert video.progress is not None
+            assert isinstance(video.url, str)
+            assert isinstance(video.state, str)
+            assert isinstance(video.progress, int)
+            assert 0 <= video.progress <= 100
 
+    def test_get_video_status(self):
+        azure_video_indexer = AzureVideoIndexer(self.account_id, self.primary_key)
+        videos = azure_video_indexer.get_videos_status([self.video_url])
+        assert videos is not None
+        assert len(videos) is 1
+        assert videos[self.video_url].url == self.video_url
+        assert videos[self.video_url].state == "Processed"
+        assert videos[self.video_url].progress == 100
 
-def test_get_video_index():
-    account_id = os.environ.get("VIDEO_INDEXER_ACCOUNT_ID")
-    primary_key = os.environ.get("VIDEO_INDEXER_PRIMARY_KEY")
-    azure_video_indexer = AzureVideoIndexer(account_id, primary_key)
-    video_id = "84505873fd"
-    response = azure_video_indexer.get_video_index(video_id)
-    assert response is not None
-    assert isinstance(response, dict)
-    assert "id" in response
-    assert response["id"] == video_id
+    def test_upload_video(self):
+        azure_video_indexer = AzureVideoIndexer(self.account_id, self.primary_key)
+        video_status = azure_video_indexer.upload_video(self.video_url, self.video_name)
+        assert video_status is not None
+        assert isinstance(video_status, tuple)
+        assert video_status.url == self.video_url
+        assert isinstance(video_status.state, str)
+
+    def test_get_video_index(self):
+        azure_video_indexer = AzureVideoIndexer(self.account_id, self.primary_key)
+        response = azure_video_indexer.get_video_index(self.video_id)
+        assert response is not None
+        assert isinstance(response, dict)
+        assert "id" in response
+        assert response["id"] == self.video_id
+
