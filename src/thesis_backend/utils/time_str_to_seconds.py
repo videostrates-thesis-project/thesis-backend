2f671e7b0e5e8de1d89f8be26e6fd8a99a49c14e
Jędrzej Głowaczewski <jedrzej.glowaczewski@gmail.com>
2024-04-24 10:16:44 +0200

    Fix converting string time received from indexer to number

--- src/thesis_backend/utils/time_str_to_seconds.py
+++ src/thesis_backend/utils/time_str_to_seconds.py
@@ -4,9 +4,10 @@ from datetime import datetime
 def time_str_to_seconds(time_str: str) -> float:
     if len(time_str) == 7:  # Format: '0:00:23'
         time_obj = datetime.strptime(time_str, "%H:%M:%S")
-    elif len(time_str) in [9, 10]:  # Format: '0:00:32.36' or '0:00:32.3'
+    elif len(time_str) > 8:  # Format: '0:00:32.36' or '0:00:32.3' or '0:00:32.3666667'
         time_obj = datetime.strptime(time_str, "%H:%M:%S.%f")
     else:
+        print(f"Invalid time format: {time_str}")
         raise ValueError("Invalid time format")
     total_seconds = (time_obj.hour * 3600) + (time_obj.minute * 60) + time_obj.second + (time_obj.microsecond / 1e6)
     return total_seconds
